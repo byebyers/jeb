5df4ec7bf4ceb907c7064ee8ea3edc46e77b35f4
byebyers <byers.jacob@gmail.com>
2022-07-22 14:41:12 -0700

    adding lists & content

--- pages/about.js
+++ pages/about.js
@@ -25,6 +25,9 @@ const query = `{
         y
       },
     },
+    clientList,
+    servicesList,
+    content,
     seo {
       ...,
       shareGraphic {
@@ -75,8 +78,50 @@ export default function About(initialData) {
               />
             </m.div>
           </m.section>
-          <m.section>
-            <h2>Services</h2>
+          <m.section className="grid grid-cols-9 gap-3 pt-80 pb-40 content-center justify-center text-2xl font-extralight">
+            <div className="col-span-9 md:col-span-1 mb-5 md:mb-0">
+              <span className="block leading-none mb-1">Clients &amp; <span className="block">Services</span></span>
+            </div>
+            
+            <div className="col-span-9 md:col-span-2 md:col-start-3 mb-6 md:mb-0">
+              {about.clientList.map((e, i) => {
+                return (
+                  <span className="block leading-none mb-1" key={i}>{e}</span>
+                )
+              })}
+            </div>
+
+            <div className="col-span-9 md:col-span-2 md:col-start-5 mb-6 md:mb-0">
+              {about.servicesList.map((e, i) => {
+                return (
+                  <span className="block leading-none mb-1" key={i}>{e}</span>
+                )
+              })}
+            </div>
+
+            <div className="col-span-9 md:col-span-2 md:col-start-8">
+              { contact.email && (<a href={`mailto:${contact.email}`} className="block leading-none mb-1 group relative overflow-hidden underline"><span className="block group-hover:translate-y-full transition-transform ease-in-out duration-300">Email</span>
+                    <span className="block absolute inset-0 transition-transform ease-in-out duration-300 -translate-y-full underline group-hover:translate-y-0">Email</span></a>)}
+
+              {contact.socials.map((e, i) => {
+                return (
+                  <a key={i} href={e.url} target="_blank" rel="noopener noreferrer" className="block leading-none mb-1 underline group relative overflow-hidden">
+                    <span className="block group-hover:translate-y-full transition-transform ease-in-out duration-300">{e.title}</span>
+                    <span className="block absolute inset-0 transition-transform ease-in-out duration-300 -translate-y-full underline group-hover:translate-y-0">{e.title}</span>
+                  </a>
+                )
+              })}
+            </div>
+          </m.section>
+          <m.section className="py-40 flex">
+            <div className="w-full md:w-4/12">
+              <span class="block text-lg mb-3 uppercase">About Me</span>
+            </div>
+            <div className="w-full md:w8/12">
+              <div className="w-10/12 md:w-8/12 xl:w-7/12 2xl:w-6/12 collab-text xl:pr-8 font-light text-2xl">
+                <BlockContent serializers={{ container: ({ children }) => children }} blocks={about.content} />
+              </div>
+            </div>
           </m.section>
         </m.main>
       </LazyMotion>
