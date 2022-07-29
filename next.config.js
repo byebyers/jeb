391ed565fcca57d8f238012c2212b59a87ae5f44
byebyers <byers.jacob@gmail.com>
2022-07-28 17:45:53 -0700

    removing preact switch

--- next.config.js
+++ next.config.js
@@ -2,19 +2,5 @@ module.exports = {
   swcMinify: true,
   images: {
     domains: ['cdn.sanity.io'],
-    loader: 'custom',
-    path: ''
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
   },
 };
