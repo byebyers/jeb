182d0d4b8c942574dfda5c28ea47da2f84413c52
byebyers <byers.jacob@gmail.com>
2022-08-10 14:33:58 -0700

    Adding log page views useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,23 @@ const revealHoriReverse = {
 export default function App({ Component, pageProps }) {
   const [introContext, setIntroContext] = useState(false);
   const router = useRouter()
+  
+  
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
 
   return (
     <>
